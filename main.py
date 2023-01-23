364dc900603bfc67a9944133a77cd20455a3fbdb
lohe040789 <lohe_35@hotmail.com>
2023-01-23 18:38:43 +0100

    [Falta 2.1-lab-numpy]

--- main.py
+++ main.py
@@ -1,67 +1,87 @@
 #1. Import the NUMPY package under the name np.
 
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
 
+print(np.__version__)
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+#1)
+a = np.random.random((2, 3, 5))
+#2)
+a = np.random.rand(2, 3, 5)
+#3)
+a = np.random.randint(1,10,size=(2, 3, 5))
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b=np.ones((5, 2, 3))
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
-
+if np.shape(a)==np.shape(b):
+        print('tienen el mismo tamañp')
+else:
+        print('No son del mismo tamaño')
 
 #8. Are you able to add a and b? Why or why not?
-
+np.add(a,b)
+#Para poder operar (sumar matrices) deben tener las mismas dimensiones a(2,3,5) y b(5,2,3) lo cual no hace posible la operacion
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = np.reshape(b, (2, 3, 5))
+print(c)
+np.shape(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+d = np.add(a , c)
+print(d)
+#cuando las matrices poseen las mismas dimensiones ya es posible realizar la operacion suma entre ellas.
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print(a)
+print(d)
 
+#Cada elemento de la matriz 'a' se suma a cada de la matriz 'd'
 
 
 #12. Multiply a and c. Assign the result to e.
-
+e=np.multiply(a , c)
+print(e)
 
 
 #13. Does e equal to a? Why or why not?
-
-
-
+np.shape(a)==np.shape(e)
+#AUNQUE e SEA EL PRODUCTO DE a Y c LOS ELEMENTOS DE DE c SON NUETROS ES DECIR MANTIENEN LOS VALORES DE a
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+d_max=d.max()
+d_min=d.min()
+d_mean=d.mean()
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f= np.empty((2,3,5), dtype=float)
 
 
 
@@ -75,7 +95,7 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-
+m=np.where(d==d_max, 100, (np.where(d < d_max) & (d > d_mean) , 75, (np.where(d == d_mean, 50 ,(np.where(d_min < d) & (d < d_mean) , 25,(np.where(d == d_min, 0, 1)))))))
 
 
 """
